ccf6fd0f9c2223edf1a9ca6838e44de1c5cbd147
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-05 13:15:23 -0300

    Update code-theo.py

--- OOP Fundamentals/code-theo.py
+++ OOP Fundamentals/code-theo.py
@@ -44,7 +44,8 @@ mc.set_count(5)
 mc.count += 1
 print(mc.count)
 #---------------------------------------------------#
-#Create your first class
+#Create your first class 
+#explore salary & name
 class Employee:
   
   def set_name(self, new_name):
@@ -65,7 +66,8 @@ emp.set_name('Korel Rossi')
 emp.set_salary(50000)
 #---------------------------------------------------#
 """ SETTER METHOD"""
-#Using attributes in class definition -> Increase salary of emp by 1500
+#Using attributes in class definition ->
+#Increase salary of emp by 1500
 class Employee:
     def set_name(self, new_name):
         self.name = new_name
@@ -85,4 +87,49 @@ emp.salary = emp.salary + 1500
 
 # Print the salary attribute of emp again
 print(emp.salary)
+#---------------------------------------------------#
+#give raise method
+class Employee:
+    def set_name(self, new_name):
+        self.name = new_name
+
+    def set_salary(self, new_salary):
+        self.salary = new_salary 
+
+    # Add a give_raise() method with raise amount as a parameter
+    def give_raise(self, amount):
+        self.salary =  self.salary + amount 
+
+emp = Employee()
+emp.set_name('Korel Rossi')
+emp.set_salary(50000)
+
+print(emp.salary)
+emp.give_raise(1500)
+print(emp.salary)
+#---------------------------------------------------#
+#month salary method
+class Employee:
+    def set_name(self, new_name):
+        self.name = new_name
+
+    def set_salary(self, new_salary):
+        self.salary = new_salary 
+
+    def give_raise(self, amount):
+        self.salary = self.salary + amount
+
+    # Add monthly_salary method that returns 1/12th of salary attribute
+    def monthly_salary(self):
+        return self.salary / 12
+    
+emp = Employee()
+emp.set_name('Korel Rossi')
+emp.set_salary(50000)
+
+# Get monthly salary of emp and assign to mon_sal
+mon_sal = emp.monthly_salary()
+
+# Print mon_sal
+print(mon_sal)
 #---------------------------------------------------#
