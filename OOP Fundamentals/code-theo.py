bb64653a7022d8780c3c05a663fa6e3550d6d854
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-05 11:49:50 -0800

    Update code-theo.py

--- OOP Fundamentals/code-theo.py
+++ OOP Fundamentals/code-theo.py
@@ -133,3 +133,61 @@ mon_sal = emp.monthly_salary()
 # Print mon_sal
 print(mon_sal)
 #---------------------------------------------------#
+"""__INIT__()"""
+class Employee:
+    # Create __init__() method
+    def __init__(self, name, salary=0):
+        # Create the name and salary attributes
+        self.name = name
+        self.salary = salary
+    
+    # From the previous lesson
+    def give_raise(self, amount):
+        self.salary += amount
+
+    def monthly_salary(self):
+        return self.salary/12
+        
+emp = Employee("Korel Rossi")
+print(emp.name)
+print(emp.salary)     
+#---------------------------------------------------#
+#Add a class constructor
+#condition constructor
+class Employee:
+
+    def __init__(self, name, salary=0):
+        self.name = name
+        # Modify code below to check if salary is positive
+        if salary > 0:
+            self.salary = salary
+        else:
+            self.salary = 0
+            print("Invalid salary!")
+
+    # ...Other methods omitted for brevity ...
+
+emp = Employee("Korel Rossi", -1000)
+print(emp.name)
+print(emp.salary)
+#---------------------------------------------------#
+#Add the hire_date attribute and set it to today's date
+class Employee:
+    
+    def __init__(self, name, salary=0):
+        self.name = name
+        if salary > 0:
+          self.salary = salary
+        else:
+          self.salary = 0
+          print("Invalid salary!")
+          
+        # Add the hire_date attribute and set it to today's date
+        self.hire_date = datetime.today()
+        
+   # ...Other methods omitted for brevity ...
+      
+emp = Employee("Korel Rossi", -1000)
+print(emp.name)
+print(emp.salary)
+
