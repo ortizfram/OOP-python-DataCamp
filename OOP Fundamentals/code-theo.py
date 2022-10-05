6184a2ac9d7957bb73f989a50457c1a7b055928a
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-05 12:44:45 -0300

    Update code-theo.py

--- OOP Fundamentals/code-theo.py
+++ OOP Fundamentals/code-theo.py
@@ -63,4 +63,26 @@ emp.set_name('Korel Rossi')
 
 # Set the salary of emp to 50000
 emp.set_salary(50000)
+#---------------------------------------------------#
+""" SETTER METHOD"""
+#Using attributes in class definition -> Increase salary of emp by 1500
+class Employee:
+    def set_name(self, new_name):
+        self.name = new_name
 
+    def set_salary(self, new_salary):
+        self.salary = new_salary 
+  
+emp = Employee()
+emp.set_name('Korel Rossi')
+emp.set_salary(50000)
+
+# Print the salary attribute of emp
+print(emp.salary)
+
+# Increase salary of emp by 1500
+emp.salary = emp.salary + 1500
+
+# Print the salary attribute of emp again
+print(emp.salary)
+#---------------------------------------------------#
