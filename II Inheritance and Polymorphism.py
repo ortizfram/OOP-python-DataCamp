d776b386ed53d68b6872d99547ce8621be6fafa4
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-06 12:16:52 -0300

    Update II Inheritance and Polymorphism.py

--- II Inheritance and Polymorphism.py
+++ II Inheritance and Polymorphism.py
@@ -1,1 +1,18 @@
+"""
+Inheritance and polymorphism are the core concepts of OOP that enable efficient and consistent
+code reuse. Learn how to inherit from a class, customize and redefine methods, and review the 
+differences between class-level data and instance-level data.
+
+  - Inheritance: extending functionality of existing code
+  - Polymorphism : creating unified interface 
+  - Encapsulation:  bundiling of data and methods
+  
+Why use class attributes? (global constants related to class ->)
+  - min / max values for attributes
+  - conmmonly used values / constants : e.g. pi for a circle class 
+Class methods ?
+  - already shared. Same code for every instance
+  - can't use instance-level data
+************************************************************************************"""
+
 
