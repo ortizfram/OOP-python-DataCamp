c79875a828eaa9b8555f1e8c931cb290139ac132
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-07 12:54:22 -0300

    Update III Integrating with Standard Python.py

--- III Integrating with Standard Python.py
+++ III Integrating with Standard Python.py
@@ -1,1 +1,8 @@
-
+"""
+//// operator & method  ////         
+      ==    __eq__()        2 objects of a class are compared using ==, (self and other), returns Boolean
+      !=    __ne__()        
+      >=    __ge__()
+      <=    __le__()
+      >     __gt__()
+      <     __lt__()
