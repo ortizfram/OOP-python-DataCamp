6d9abcf47029d9343854ebe8a0831ad4b7f997a6
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-07 14:13:36 -0300

    Update III Integrating with Standard Python.py

--- III Integrating with Standard Python.py
+++ III Integrating with Standard Python.py
@@ -28,4 +28,18 @@ print(acct1 == acct2)
 print(acct1 == acct3)
 #----------------------------------------------------------------------------------#
 #Checking class equality
+class BankAccount:
+    def __init__(self, number, balance=0):
+        self.number, self.balance = number, balance
+      
+    def withdraw(self, amount):
+        self.balance -= amount 
 
+    # MODIFY to add a check for the type()
+    def __eq__(self, other):
+        return (self.number == other.number) and \
+        (type (self) == type(other) )
+
+acct = BankAccount(873555333)
+pn = Phone(873555333)
+print(acct == pn)
