667120a5c2d98800f9819627964c804b8c2e8e9d
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-07 11:03:10 -0300

    Update II Inheritance and Polymorphism.py

--- II Inheritance and Polymorphism.py
+++ II Inheritance and Polymorphism.py
@@ -185,3 +185,26 @@ mng = Manager('Debbie Lashko', 86500)
 print(mng.name)
 print(mng.salary)
 #------------------------------------------------------------------------#
+#custom child class
+class Employee:
+    def __init__(self, name, salary=30000):
+        self.name = name
+        self.salary = salary
+
+    def give_raise(self, amount):
+        self.salary += amount
+
+        
+class Manager(Employee):
+  # Add a constructor 
+    def __init__(self, name, salary=50000, project= None):
+
+        # Call the parent's constructor   
+        Employee.__init__(self, name, salary)
+
+        # Assign project attribute
+        self.project = project
+
+  
+    def display(self):
+        print("Manager ", self.name)
