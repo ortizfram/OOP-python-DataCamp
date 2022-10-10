0ecf81417e9b269ef1905f2e5a7a662121215670
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-10 09:34:49 -0300

    Update IV Best Practices of Class Design.py

--- IV Best Practices of Class Design.py
+++ IV Best Practices of Class Design.py
@@ -47,3 +47,38 @@ What went wrong with these classes
 # option 3 :
 The 4x4 Square object would no longer be a square if we assign 7 to h."""
 #-------------------------------------------------------------------------#
+# 2.c
+class Rectangle:
+    def __init__(self, w,h):
+      self.w, self.h = w,h
+
+# Define set_h to set h      
+    def set_h(self, h):
+      self.h = h
+      
+# Define set_w to set w          
+    def set_w(self,w):
+      self.w = w
+      
+#set both attributes to w      
+class Square(Rectangle):
+    def __init__(self, w):
+      self.w, self.h = w, w 
+e
+# Define set_h to set w to h attribute
+    def set_h(self, h):
+      self.h = h
+      self.w = h
+
+# Define set_w to set h wo w attribute    
+    def set_w(self,w):
+      self.w= w
+      self.h= w 
+#-------------------------------------------------------------------------#
+# 2.d
+"""How does using these setter methods violate Liskov Substitution principle?
+option 2 : 
+
+Each of the setter methods of Square change both h and w attributes, while 
+setter methods of Rectangle change only one attribute at a time, so the Square 
+objects cannot be substituted for Rectangle into programs that rely on one attribute staying constant."""
