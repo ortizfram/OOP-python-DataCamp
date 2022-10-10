ae65252eef99eff8f7397c8f8b55669bdec20012
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-10 09:03:49 -0300

    Update IV Best Practices of Class Design.py

--- IV Best Practices of Class Design.py
+++ IV Best Practices of Class Design.py
@@ -1,1 +1,28 @@
-"""   - Polymorphism : using unified interface to operate on objects or different clases  """
+"""   - Polymorphism : using unified interface to operate on objects or different clases 
+*****************************************************************************************************"""
+# Polymorphic methods
+# identify the output
+class Parent:
+    def talk(self):
+        print("Parent talking!")     
+
+class Child(Parent):
+    def talk(self):
+        print("Child talking!")          
+
+class TalkativeChild(Parent):
+    def talk(self):
+        print("TalkativeChild talking!")
+        Parent.talk(self)
+
+
+p, c, tc = Parent(), Child(), TalkativeChild()
+
+for obj in (p, c, tc):
+    obj.talk()
+"""output : 
+Parent talking!
+Child talking!
+Talkative Child talking!
+Parent talking!"""
+#-------------------------------------------------------------------------#
